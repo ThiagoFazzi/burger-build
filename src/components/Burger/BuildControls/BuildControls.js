13aa4c9c0eec8f16d3fd84ab59adc30619a0f72c
ThiagoFazzi <thiago@xtwo.com.br>
2019-01-03 15:03:10 +0100

    Add BuildControls component at BurgerBuider component

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import classes from './BuildControls.module.css'
+import BuildControl from './BuildControl/BuildControl'
+
+const controls = [
+  {label: 'Salad', type: 'salad'},
+  {label: 'Bacon', type: 'bacon'},
+  {label: 'Meat', type: 'meat'},
+  {label: 'Cheese', type: 'cheese'}
+]
 
 const buildControls = (props) => {
   return (
-    <div>
-      
+    <div className={classes.BuildControls}>
+      {controls.map(crtl => (
+        <BuildControl key={crtl.label} label={crtl.label}  />
+      ))}
     </div>
   )
 }
