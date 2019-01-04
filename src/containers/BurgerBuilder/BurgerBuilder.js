77b0f1f8af73edd9a63c536e6746b6652f83050f
ThiagoFazzi <thiago@xtwo.com.br>
2019-01-04 14:07:51 +0100

    implemented method updatePurchaseState at BurgerBuilder container to enable or not the ORDER NOW button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,6 +21,20 @@ class BurgerBuilder extends Component {
     purchaseble: false
   }
 
+  updatePurchaseState() {
+    const ingredients = {
+      ...this.state.ingredients
+    }
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey]
+      })
+      .reduce((sum, el) => {
+        return sum + el
+      },0)
+
+  }
+
   addIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type]
     const updatedCount= oldCount + 1
