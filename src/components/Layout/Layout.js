f730d32cfc8eadefdebaff9262c1149e782f78d2
ThiagoFazzi <thiago@xtwo.com.br>
2019-01-06 13:35:31 +0100

    implemented toggle feature to SideDrop component through Toobar component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -13,10 +13,16 @@ class Layout extends Component {
     this.setState({showSideDrawer: false})
   }
 
+  sideDrawerToggleClickedHandler = () => {
+    this.setState((prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer}
+    })
+  }
+
   render(){
     return(
       <>
-        <Toobar/>
+        <Toobar drawerToggleClicked={this.sideDrawerToggleClickedHandler}/>
         <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
         <main className={classes.Content}>
           {this.props.children}
