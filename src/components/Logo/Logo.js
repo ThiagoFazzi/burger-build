01257f5e21fee09742c9e2cbcef8268a95812849
ThiagoFazzi <thiago@xtwo.com.br>
2019-01-06 10:39:28 +0100

    implement Logo css file also call that on the Logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import burgerLogo from '../../assets/images/burger-logo.png'
 
 
 const logo = (props) => (
-  <div>
+  <div className={classes.Logo}>
     <img src={burgerLogo} alt='Burger Shop' />
   </div>
 )
