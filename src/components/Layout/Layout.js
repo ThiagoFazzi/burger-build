90c1d279d5456a9829df8fb826fa4b7647f870c5
ThiagoFazzi <thiago@xtwo.com.br>
2019-01-06 13:08:50 +0100

    Turned Layout component on container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './Layout.module.css'
 import Toobar from '../Navigation/Toobar/Toobar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => {
-  return(
-    <>
-      <Toobar/>
-      <SideDrawer />
-      <main className={classes.Content}>
-        {props.children}
-      </main>
-    </>
-  )
+class Layout extends Component {
+  render(){
+    return(
+      <>
+        <Toobar/>
+        <SideDrawer />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </>
+    )
+  }
 }
 
-export default layout
+export default Layout
