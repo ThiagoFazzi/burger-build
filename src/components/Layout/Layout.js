75b88c8a510ebda25f28948e4788acb7e15a8287
ThiagoFazzi <thiago@xtwo.com.br>
2019-01-06 11:52:33 +0100

    imported SideDrawer component in Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import classes from './Layout.module.css'
 import Toobar from '../Navigation/Toobar/Toobar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = (props) => {
   return(
     <>
       <Toobar/>
+      <SideDrawer />
       <main className={classes.Content}>
         {props.children}
       </main>
