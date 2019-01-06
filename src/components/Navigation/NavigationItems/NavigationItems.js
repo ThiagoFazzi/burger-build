8c39750956aa77386b141e7ee7f6c7158626c94d
ThiagoFazzi <thiago@xtwo.com.br>
2019-01-06 11:13:00 +0100

    added NavigationItem component inside NavigationItems component

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import classes from './NavigationItems.module.css'
+import NavigationItem from './NavigationItem/NavigationItem'
 
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
-    
+    <NavigationItem />
   </ul>
 )
 
