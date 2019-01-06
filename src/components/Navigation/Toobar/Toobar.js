15a2f3b575c9a86e76cf1762daea40670614804d
ThiagoFazzi <thiago@xtwo.com.br>
2019-01-06 11:24:58 +0100

    called NavigationItems component in Toolbar component

--- src/components/Navigation/Toobar/Toobar.js
+++ src/components/Navigation/Toobar/Toobar.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import classes from './Toobar.module.css'
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toobar = (props) => (
   <header className={classes.Toobar}>
     <div>MENU</div>
     <Logo />
-    <ul>
-      ...
-    </ul>
+    <NavigationItems />
   </header>
 )
 
