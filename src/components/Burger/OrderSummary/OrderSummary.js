93f65b6101cbe0a1f983421bf7c61fe92391b3a7
ThiagoFazzi <thiago@xtwo.com.br>
2019-01-06 14:22:42 +0100

    update performance on Modal component to not re-render if not necessary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
   
-  const ingredientsSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return(
-        <li key={igKey}>
-          <span style={{textTransform: 'capitalize'}}>{igKey}</span> : {props.ingredients[igKey]}
-        </li>
-      )
-    })
+    render(){
+      const ingredientsSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
+        return(
+          <li key={igKey}>
+            <span style={{textTransform: 'capitalize'}}>{igKey}</span> : {this.props.ingredients[igKey]}
+          </li>
+        )
+      })
 
-  return(
-    <>
-      <h3>Your Order</h3>
-      <p>The delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientsSummary}
-      </ul>
-      <p><strong>Total price: {props.price}</strong></p>
-      <p>Continue to checkout?</p>
-      <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-    </>
-  )
+      return(
+        <>
+          <h3>Your Order</h3>
+          <p>The delicious burger with the following ingredients:</p>
+          <ul>
+            {ingredientsSummary}
+          </ul>
+          <p><strong>Total price: {this.props.price}</strong></p>
+          <p>Continue to checkout?</p>
+          <Button btnType='Danger' clicked={this.props.purchaseCancelled}>CANCEL</Button>
+          <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+        </>
+      )
+    }
+
+
 }
 
-export default orderSummary
+export default OrderSummary
