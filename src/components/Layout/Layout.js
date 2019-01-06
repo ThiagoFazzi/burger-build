099d91a4c2b8c816d035d05305baaf826662aa95
ThiagoFazzi <thiago@xtwo.com.br>
2019-01-06 10:24:49 +0100

    add Toobar component also Toobar css file component scoped

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import classes from './Layout.module.css'
+import Toobar from '../Navigation/Toobar/Toobar'
 
 const layout = (props) => {
   return(
     <>
-      <div>Toobar, Sidebar, Backdrop</div>
+      <Toobar/>
       <main className={classes.Content}>
         {props.children}
       </main>
---
+++ src/components/Navigation/Toobar/Toobar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './Toobar.module.css'
+
+const toobar = (props) => (
+  <header className={classes.Toobar}>
+    <div>MENU</div>
+    <div>LOGO</div>
+    <ul>
+      ...
+    </ul>
+  </header>
+)
+
+export default toobar
