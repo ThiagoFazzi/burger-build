fc2ec120a4201e330dc287cf75163c41ef2e5c58
ThiagoFazzi <thiago@xtwo.com.br>
2019-01-07 15:57:11 +0100

    Updates

---
+++ precache-manifest.31972c8396c6d9d74d7cb830c94778fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-build/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "8fedf52bdd50fe24eb8e",
+    "url": "/burger-build/static/js/runtime~main.8fedf52b.js"
+  },
+  {
+    "revision": "07e1dd59238579de06fc",
+    "url": "/burger-build/static/js/main.07e1dd59.chunk.js"
+  },
+  {
+    "revision": "1c53900a0d0de3b2dc60",
+    "url": "/burger-build/static/js/1.1c53900a.chunk.js"
+  },
+  {
+    "revision": "07e1dd59238579de06fc",
+    "url": "/burger-build/static/css/main.df6ea820.chunk.css"
+  },
+  {
+    "revision": "cd284fff384ad73568593552e660b9dd",
+    "url": "/burger-build/index.html"
+  }
+];
